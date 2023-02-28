d7909ec9ecdd01601ad5916a7d965b6020741b4f
Ilya <ballukrainian@gmail.com>
2023-02-28 09:07:24 +0200

    add Categories

--- articles/models.py
+++ articles/models.py
@@ -5,6 +5,14 @@ class Article(models.Model):
     title = models.CharField(max_length=255)
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title
