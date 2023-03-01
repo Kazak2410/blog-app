2d008a3b5027c77da6adc198e467e9b082eb41d5
Ilya <ballukrainian@gmail.com>
2023-03-01 09:54:04 +0200

    work with styles

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from articles.models import Article
 
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
+    model = Article
     template_name = 'home_page.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about_page.html'
