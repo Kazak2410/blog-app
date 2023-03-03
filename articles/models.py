5ad70bf1609da96329d09711cfc47207a0488a15
Ilya <ballukrainian@gmail.com>
2023-03-03 09:45:39 +0200

    add images and page about myself

--- articles/models.py
+++ articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Article(models.Model):
@@ -6,9 +7,13 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    img = models.ImageField(upload_to='D:\\projects\\blog\\static\\images')
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        reverse('article_detail', args=[str(self.id)])
 
 
 class Category(models.Model):
